ec5e442515ea0a1c2823d559181de5398674fa59
Francesco Antonio Barletta <franchebarletta@gmail.com>
2025-06-11 22:53:21 -0300

    format

--- bits.c
+++ bits.c
@@ -2,23 +2,23 @@
 
 int main()
 {
-	int x, y;
-	int width = 500, height = 500;
+    int x, y;
+    int width = 500, height = 500;
 
-	printf("P1\n");
-	printf("# Created by firstimage.c\n");
-	printf("%d %d\n", width, height);
-	printf("255\n");
+    printf("P1\n");
+    printf("# Created by firstimage.c\n");
+    printf("%d %d\n", width, height);
+    printf("255\n");
 
-	for (y = 0; y < height; y++)
-	{
-		for (x = 0; x < width; x++)
-		{
-			printf("%d ", ((x % 50) * (y % 50)) % 256);
-		}
-	}
+    for (y = 0; y < height; y++)
+    {
+        for (x = 0; x < width; x++)
+        {
+            printf("%d ", ((x % 50) * (y % 50)) % 256);
+        }
+    }
 
-	return 0;
+    return 0;
 }
 /*
 Codigo igual a pixel_squares pero solo cambie P1/n el cual el mismo solo toma valores blanco y negro, blanco = 0, negro = 1.
